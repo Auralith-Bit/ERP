2c36d2c5a2097dde4830b1b08400a8a7aa2a0e64
sushmi4 <tsushmita360@gmail.com>
2026-06-03 16:12:17 +0545

    fixed medias

--- auralith_erp/wsgi.py
+++ auralith_erp/wsgi.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 
 import os
 
+from django.conf import settings
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'auralith_erp.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root=settings.STATIC_ROOT)
+application.add_files(settings.MEDIA_ROOT, prefix='media/')
